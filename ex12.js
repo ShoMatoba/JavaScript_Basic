4f86769ce628aa791ae19b8111b076489e01e5c2
SHO MATOBA <2004240018ms@edu.oca.ac.jp>
2023-01-28 17:22:16 +0900

    ew

---
+++ ex12.js
@@ -0,0 +1,4 @@
+test_array = [1,2,3]
+new_array = test_array.map(element=>element+'追加文言')
+q_array = test_array.filter(element=>(element === 2))
+find_result = test_array.find(element=>(element === 3))
