58e67bb7ab97ae7435749cd8accdb9ba82d65f19
SHO MATOBA <2004240018ms@edu.oca.ac.jp>
2023-01-28 17:34:09 +0900

    heghe

--- ex12.js
+++ ex12.js
@@ -2,3 +2,5 @@ test_array = [1,2,3]
 new_array = test_array.map(element=>element+'追加文言')
 q_array = test_array.filter(element=>(element === 2))
 find_result = test_array.find(element=>(element === 3))
+
+new_array2 = test_array.map(element=>element+'追加文言').filter(element=>element.startsWith("2"))
