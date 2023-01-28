c86604704ba0b003b5452fc8c0ea4381d345cb6a
SHO MATOBA <2004240018ms@edu.oca.ac.jp>
2023-01-28 15:35:31 +0900

    bvvv

---
+++ ex10.js
@@ -0,0 +1,14 @@
+const pom = (2)=>{
+const pom = (2)=>{
+
+const pom = (2)=>{console.log(2);
+const pom = (2)=>{console.log(2);return a;}
+const myError = Error("This is My Error");
+throw my Error;
+try{
+throw myError;
+}catch(e){
+console.log(e);
+}finally{
+console.log('後処理');
+}
