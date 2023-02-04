ada18124b79cb5475fe54ffa7b015af1ddb906f2
SHO MATOBA <2004240018ms@edu.oca.ac.jp>
2023-02-04 17:34:06 +0900

    .

---
+++ try_typescript/src/test_export.ts
@@ -0,0 +1,2 @@
+export const test_name = "ExportName";
+export const test_age = 23;
---
+++ try_typescript/src/test_import.ts
@@ -0,0 +1,2 @@
+import {test_name, test_age} from "./test_export.js" //←注意!!tsでなくjsにすること!!
+console.log(test_name, test_age);
